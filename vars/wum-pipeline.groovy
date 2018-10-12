0a66f727494aeb740fd96098ed9c66619f78a9e5
RidmiR <ridmilokumudali@WSO2s-MacBook-Pro.local>
2018-10-12 15:00:01 +0530

    Add comments in the code

--- vars/wum-pipeline.groovy
+++ vars/wum-pipeline.groovy
@@ -3,10 +3,16 @@ import groovy.json.*
 pipeline {
     agent any
     environment {
+        //Set the jq path
         PATH = "/usr/local/Cellar/jq/1.5_3/bin:$PATH"
+        
+        //Testgrid LDAP username and password
         OS_USERNAME = credentials('OS_USERNAME')
         OS_PASSWORD = credentials('OS_PASSWORD')
+
+        //WUM UAT Appkey
         UAT_APPKEY = credentials('UAT_APPKEY')
+        //WUM LIVE Appkey
         LIVE_APPKEY = credentials('LIVE_APPKEY')
         PWD = pwd()
         JOB_CONFIG_YAML = "job-config.yaml"
@@ -35,11 +41,14 @@ pipeline {
                           echo UAT_APPKEY : ${UAT_APPKEY}
                           echo LIVE_APPKEY : ${LIVE_APPKEY}
                          
+                          #clon the apim integration repo which contains the WUM script
                           cd ${SCENARIOS_LOCATION}
                           if [ ! -d "${SCENARIOS_LOCATION}/apim-test-integration" ] ; then
                               git clone ${SCENARIOS_REPOSITORY}
                           fi
                           cd ${SCENARIOS_LOCATION}
+
+                          # Start running WUM script which have the whole procees of calling to WUM UAT and get the Products, Version and channel list
                           mkdir wum_script
                           cp -f ${SCENARIOS_LOCATION}/apim-test-integration/newshell.sh ${SCENARIOS_LOCATION}/wum_script
                           cd ${SCENARIOS_LOCATION}/wum_script 
@@ -53,18 +62,19 @@ pipeline {
             steps {
                 script {
                     try{
+                        //Converting job name list text as a json file
                         sh """
                             jq -Rs '[ split("\n")[] | select(length > 0)
                                 | split(",") | {JobName: .[0]} ]' "${SCENARIOS_LOCATION}/wum_script/job-list.txt" > "${SCENARIOS_LOCATION}/wum_script/job-list.json"
                          """
                         def inputFile = new File("${SCENARIOS_LOCATION}/wum_script/job-list.json")
-
                         def jsonSulper = new JsonSlurper()
                         def InputJSON = jsonSulper.parseText(inputFile.text)
                         jsonSulper = null
 
                         def wumJobs = [:]
 
+                        //Get the job name from the job-list.json and trigger builds pararelly
                         for (int index = 0; index < InputJSON.size(); index++) {
                             String jobName = InputJSON[index].JobName;
                             wumJobs["job:"+ jobName] = {build job: jobName};
@@ -79,6 +89,8 @@ pipeline {
                         echo "Few of the builds are not found to trigger."
 
                     }
+
+                    //Delete the created directory which have the all the text files.
                     dir("${SCENARIOS_LOCATION}/wum_script") {
                         deleteDir()
                     }
