41ec3a01216b5587e5414f0b6cb4c1e1725d7cee
RidmiR <ridmi@wso2.com>
2019-01-09 10:27:53 +0530

    Add changes

--- src/org/wso2/tg/jenkins/executors/TestExecutor.groovy
+++ src/org/wso2/tg/jenkins/executors/TestExecutor.groovy
@@ -147,27 +147,6 @@ def prepareWorkspace(testPlanId, scenarioConfigs) {
         ls ${props.WORKSPACE}/${testPlanId}/
     """
 
-//    if ("${props.TEST_MODE}" == 'WUM') {
-        for (repo in scenarioConfigs) {
-
-//            echo "TEST_MODE -------->>>>>>>>>   :  ${props.TEST_MODE}"
-//            echo "PATH --------->>>>>>>>>>>>   =========>>>>>    : ${props.WORKSPACE}/${testPlanId}/workspace/${props.SCENARIOS_LOCATION}/${repo.get("dir")}/${repo.get("dir")}"
-
-            // Providing nexus settings xml as a config file provider through Jenkins.
-            // This is using only in WUM Test mode.
-
-            configFileProvider(
-                    [configFile(fileId: "uat-nexus-settings", targetLocation:
-                            "${props.WORKSPACE}/${testPlanId}/workspace/${props.SCENARIOS_LOCATION}/${repo.get("dir")}/${repo.get("dir")}/uat-nexus-settings.xml")]) {
-            }
-//            sh """
-//                ls ${props.WORKSPACE}/${testPlanId}/workspace/${props.SCENARIOS_LOCATION}/${repo.get("dir")}/${repo.get("dir")}
-//
-//            """
-        }
-
-//    }
-
     tryAddKnownHost("github.com")
     cloneRepo(props.INFRASTRUCTURE_REPOSITORY_URL, props.INFRASTRUCTURE_REPOSITORY_BRANCH, props.WORKSPACE + '/' +
             testPlanId + '/workspace/' + props.INFRA_LOCATION)
