70b836befe62c87bf935bb7590169bd830067f65
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-06-13 15:06:12 +0530

    Add product name to the rds resource tag

--- kubernetes/product-deployment/scripts/create-database.sh
+++ kubernetes/product-deployment/scripts/create-database.sh
@@ -40,7 +40,7 @@ else
     exit 1;
 fi;
 
-aws cloudformation create-stack --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME}   --template-body file://testgrid-rds-cf.yaml --parameters ParameterKey=pDbUser,ParameterValue="${DB_USERNAME}" ParameterKey=pDbPass,ParameterValue="${DB_PASSWORD}"  ParameterKey=pDbEngine,ParameterValue="$dbEngine" ParameterKey=pDbVersion,ParameterValue="${db_version}" ParameterKey=pDbInstanceClass,ParameterValue="${db_instance_class}" || { echo 'Failed to create RDS stack.';  exit 1; }
+aws cloudformation create-stack --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME}   --template-body file://testgrid-rds-cf.yaml --parameters ParameterKey=pDbUser,ParameterValue="${DB_USERNAME}" ParameterKey=pDbPass,ParameterValue="${DB_PASSWORD}"  ParameterKey=pDbEngine,ParameterValue="$dbEngine" ParameterKey=pDbVersion,ParameterValue="${db_version}" ParameterKey=pDbInstanceClass,ParameterValue="${db_instance_class}"  ParameterKey=pProductName,ParameterValue="${product_name}-testgrid" || { echo 'Failed to create RDS stack.';  exit 1; }
 
 # Wait for RDS DB to come alive.
 aws cloudformation wait stack-create-complete --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME} || { echo 'RDS stack creation timeout.';  exit 1; }
