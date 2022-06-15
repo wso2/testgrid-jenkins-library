a37de2da0316b5bfa6e75853875b06e93d3e8b5a
Tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2022-06-15 11:06:46 +0530

    Remove returned True from bash commands

--- kubernetes/product-deployment/scripts/cleanup.sh
+++ kubernetes/product-deployment/scripts/cleanup.sh
@@ -20,8 +20,8 @@
 
 productName=$1;
 echo "Scaling node group instances to zero."
-eksctl scale nodegroup --region ${EKS_CLUSTER_REGION} --cluster ${EKS_CLUSTER_NAME} --name ng-1 --nodes=0 || true
-sh ./kubernetes/product-deployment/scripts/"$productName"/cleanup.sh || true
+eksctl scale nodegroup --region ${EKS_CLUSTER_REGION} --cluster ${EKS_CLUSTER_NAME} --name ng-1 --nodes=0
+sh ./kubernetes/product-deployment/scripts/"$productName"/cleanup.sh
 echo "Deleting RDS database."
-aws cloudformation delete-stack --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME} || true
-aws cloudformation wait stack-delete-complete --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME} || true
+aws cloudformation delete-stack --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME}
+aws cloudformation wait stack-delete-complete --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME}
