27627ba892c952b051c11a0054d0c19eacbac1e5
AnuGayan <anushaj@wso2.com>
2023-10-09 12:06:29 +0530

    Support both group execution and full test execution

--- scripts/intg-test-deployment.sh
+++ scripts/intg-test-deployment.sh
@@ -22,11 +22,13 @@ deploymentName=$1
 productRepository=$2
 productTestBranch=$3
 productTestScript=$4
+productTestGroup=$5
 currentScript=$(dirname $(realpath "$0"))
 
 deploymentDirectory="${WORKSPACE}/deployment/${deploymentName}"
 parameterFilePath="${deploymentDirectory}/parameters.json"
 testOutputDir="${deploymentDirectory}/outputs"
+productDirectory="product-apim"
 
 source ${currentScript}/common-functions.sh
 
@@ -38,16 +40,25 @@ function cloneTestRepo(){
     local cloneString=$(echo ${productRepository} | sed  's#https://#&'${githubUsername}':'${githubPassword}@'#')
 
     log_info "Cloning product repo to get test scripts"
-    git -C ${deploymentDirectory} clone ${cloneString} --branch ${productTestBranch}
-    if [[ $? != 0 ]];
+    log_info "Product repo ${productRepository}"
+
+    if [ ! -d  "${deploymentDirectory}/${productDirectory}" ];
+     then
+      git -C ${deploymentDirectory} clone ${cloneString} --branch ${productTestBranch}
+      if [[ $? != 0 ]];
         then
-            log_error "Testing repo clone failed! Please check if the Git credentials or the test repo name is correct."
-            bash ${currentScript}/post-actions.sh ${deploymentName}
-            exit 1
+          log_error "Testing repo clone failed! Please check if the Git credentials or the test repo name is correct."
+          bash ${currentScript}/post-actions.sh ${deploymentName}
+          exit 1
         else
-            log_info "Cloning the test repo was successfull!"
-        fi
+          log_info "Cloning the test repo was successfull!"
+      fi
+    fi
+
     local repoName="$(basename ${productRepository} .git)"
+
+    log_info "Product repo name ${repoName}"
+
     productDirectoryLocation="${deploymentDirectory}/${repoName}"
 }
 
@@ -58,12 +69,11 @@ function deploymentTest(){
         rm -r "${testOutputDir}"
     fi
     mkdir ${testOutputDir}
-    log_info "Executing scenario tests!"
-    bash ${currentScript}/intg-test-executer.sh "${deploymentDirectory}" "${testOutputDir}"
+    log_info "Executing scenario tests for ${productTestGroup}!"
+    bash ${currentScript}/intg-test-executer.sh "${deploymentDirectory}" "${testOutputDir}" "${productTestGroup}"
     if [[ $? != 0 ]];
     then
         log_error "Executing post actions!"
-        bash ${currentScript}/post-actions.sh ${deploymentName}
         exit 1
     else
         log_info "Test Execution Passed!"
