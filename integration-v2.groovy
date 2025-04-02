6fdfd3b33ff293f7cca4f55ee14181e0498197e7
Kavinda Rajapakse <kavindasr@gmail.com>
2025-04-02 12:20:50 +0530

    Update APIM test integration repository URL in integration-v2.groovy

--- integration-v2.groovy
+++ integration-v2.groovy
@@ -59,7 +59,7 @@ String helmRepoUrl = "https://github.com/kavindasr/helm-apim.git"
 String helmRepoBranch = "apim-intg"
 String helmDirectory = "helm-apim"
 // APIM Test Integration repository details
-String apimIntgRepoUrl = "https://github.com/wso2/apim-test-integration.git"
+String apimIntgRepoUrl = "https://github.com/kavindasr/apim-test-integration.git"
 String apimIntgRepoBranch = "4.5.0-profile-automation"
 String apimIntgDirectory = "apim-test-integration"
 
