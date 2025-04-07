128ff2bc82e15897ccb4d088abc49e7d36bcaa51
Kavinda Rajapakse <kavindasr@gmail.com>
2025-04-07 20:53:50 +0530

    Remove unnecessary directory listing commands from docker build stage

--- docker-builder.groovy
+++ docker-builder.groovy
@@ -185,8 +185,6 @@ pipeline {
                             String UPDATED_PRODUCT_PACK_HOST_LOCATION_URL = "http://localhost:8889"
                             dir("${dockerDirectory}") {
                                 sh """
-                                pwd
-                                ls -la
                                 cd dockerfiles/${os}/${product_name_map[wso2_product]}
                                 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} ${docker_registry}
                                 docker build -t ${docker_registry}/${wso2_product}:${tag} . --build-arg WSO2_SERVER_DIST_URL=${UPDATED_PRODUCT_PACK_HOST_LOCATION_URL}/${wso2_product}-${wso2_product_version}.zip
