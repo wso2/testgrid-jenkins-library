0300707ec8956658b458d20dd33b9e72e82ec07e
Kavinda Rajapakse <kavindasr@gmail.com>
2025-04-15 17:43:52 +0530

    Fix NVM source command syntax in pipeline script for improved compatibility

--- integration-v2.groovy
+++ integration-v2.groovy
@@ -705,7 +705,7 @@ pipeline {
                                         sh """
                                             #!/bin/bash
                                             export NVM_DIR="$HOME/.nvm"
-                                            [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+                                            [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
                                             # Install Node.js v19 if not present
                                             if ! nvm ls 19 >/dev/null 2>&1; then
