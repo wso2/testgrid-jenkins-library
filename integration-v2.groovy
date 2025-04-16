ea94d38273cfe6a35f93d4db6cf3dceaeeef8a7e
Kavinda Rajapakse <kavindasr@gmail.com>
2025-04-16 12:33:38 +0530

    Add JKS secret creation to deployment pipeline

--- integration-v2.groovy
+++ integration-v2.groovy
@@ -590,6 +590,12 @@ pipeline {
 
                                             # Create a namespace for the deployment
                                             kubectl create namespace ${namespace} || echo "Namespace ${namespace} already exists."
+
+                                            aws s3 cp --quiet s3://wum-for-testgrid/testgrid-intg/client-truststore.jks .
+                                            aws s3 cp --quiet s3://wum-for-testgrid/testgrid-intg/wso2carbon.jks .
+
+                                            # Create jks-secret
+                                            kubectl create secret generic jks-secret --from-file=wso2carbon.jks --from-file=client-truststore.jks -n ${namespace}
                                         """
                                         println "Namespace created: ${namespace}"
 
