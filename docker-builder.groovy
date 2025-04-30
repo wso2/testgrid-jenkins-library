dbdb6c3a29755ae47bb705e73fd4a10eed8390cb
Kavinda Rajapakse <kavindasr@gmail.com>
2025-04-30 09:42:25 +0530

    Replace with single =

--- docker-builder.groovy
+++ docker-builder.groovy
@@ -117,9 +117,8 @@ pipeline {
                         }
                         def statusCode = sh(
                                 script: """
-                                #!/bin/bash
                                 echo "${use_staging}"
-                                if [ "${use_staging}" == "true" ] || [ "${use_staging}" == true ]; then
+                                if [ "${use_staging}" = "true" ] || [ "${use_staging}" = true ]; then
                                     export WSO2_UPDATES_UPDATE_LEVEL_STATE=TESTING
                                 else
                                     export WSO2_UPDATES_UPDATE_LEVEL_STATE=VERIFYING
