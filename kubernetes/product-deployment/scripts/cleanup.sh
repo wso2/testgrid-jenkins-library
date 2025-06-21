76a78fed13457bbfb45cbe0f8d9257ffd9cca64d
Kavinda Rajapakse <kavindasr@gmail.com>
2025-06-21 12:58:26 +0530

    Remove scaling node group instances command from cleanup script

--- kubernetes/product-deployment/scripts/cleanup.sh
+++ kubernetes/product-deployment/scripts/cleanup.sh
@@ -20,8 +20,6 @@
 
 productName=$1;
 ./kubernetes/product-deployment/scripts/"$productName"/cleanup.sh
-echo "Scaling node group instances to zero."
-# eksctl scale nodegroup --region ${EKS_CLUSTER_REGION} --cluster ${EKS_CLUSTER_NAME} --name ng-1 --nodes=0
 echo "Deleting RDS database."
 aws cloudformation delete-stack --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME}
 aws cloudformation wait stack-delete-complete --region ${EKS_CLUSTER_REGION} --stack-name ${RDS_STACK_NAME}
